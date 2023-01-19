531cd7e5645e6504719a47dabe38de3fd0e491b0
Roy <roy@osherove.com>
2023-01-19 15:37:47 +0200

    show mumbai

--- lib/constants.ts
+++ lib/constants.ts
@@ -44,5 +44,7 @@ goerli.iconUrl = '/icons/NetworkEthereumTest.svg'
 sepolia.iconUrl = '/icons/NetworkEthereumTest.svg'
 
 export const ETH_CHAINS = [mainnet, polygon, optimism, arbitrum, goerli]
-export const ETH_CHAINS_PROD = [mainnet, polygon, optimism, arbitrum]
-export const ETH_CHAINS_TEST = [polygonMumbai, mainnet, polygon, optimism, arbitrum, goerli, sepolia, hardhat]
+// export const ETH_CHAINS_PROD = [mainnet, polygon, optimism, arbitrum]
+export const ETH_CHAINS_PROD = [polygonMumbai,mainnet, polygon, optimism, arbitrum]
+// export const ETH_CHAINS_TEST = [polygonMumbai, polygon, optimism, arbitrum, goerli, sepolia, hardhat]
+export const ETH_CHAINS_TEST = [polygonMumbai, hardhat]
