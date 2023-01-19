5cad7dca0604f1bc71982f4a1d8f5e1998c74dfb
Roy <roy@osherove.com>
2023-01-19 13:05:43 +0200

    remove ky-universal

--- lib/actions/getAppUsers.ts
+++ lib/actions/getAppUsers.ts
@@ -1,14 +1,11 @@
-import kyUniversal from 'ky-universal'
+// import kyUniversal from 'ky-universal'
 
 export async function getAppUsers(_params?: BlockPagination): Promise<
-  | {
-      users?: Array<any>
-    }
-  | undefined
-  | void
-> {
+  | { users?: Array<any> } | undefined | void >
+   {
   try {
-    return await kyUniversal('/api/app/users').json()
+    return undefined;
+    // return await kyUniversal('/api/app/users').json()
   } catch (error: any) {
     throw error
   }
