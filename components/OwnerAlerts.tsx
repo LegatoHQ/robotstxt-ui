a0f247ce78e6aca8e488d4647940e368afc1dd44
Roy <roy@osherove.com>
2023-03-07 23:21:49 +0200

    error message clearer

--- components/OwnerAlerts.tsx
+++ components/OwnerAlerts.tsx
@@ -14,12 +14,19 @@ export const OwnerAlerts: FC<{ isIdle: boolean; isLoading: boolean; isSuccess: b
   if (isError)
     return (
       <div className="flex flex-col items-center gap-2 dark:text-white">
-        <div className="flex items-center gap-2">
           <div className="text-red-500">
             <X size={24} />
           </div>
-          <div className="text-sm font-semibold">Something went wrong</div>
-          <div className="text-sm font-semibold">please try different address</div>
+        <div className="grid items-center gap-2">
+          <div className="text-sm font-semibold">Something went wrong.</div>
+          <div className="text-sm font-semibold">Did you connect your wallet? (bottom right) </div>
+          <div className="text-sm font-semibold">If so, are you on the right network? (bottom , second from the right)</div>
+          <a
+            target={'_blank'}
+            href="https://docs.robots-txt.xyz/how-to-use/something-went-wrong"
+            className="text-sm font-semibold text-blue-400 underline">
+            or seek help
+          </a>
         </div>
       </div>
     )
